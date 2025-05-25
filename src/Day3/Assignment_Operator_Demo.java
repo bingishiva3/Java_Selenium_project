1b7234bd65267f41249ce28e32168ca5388761d7
Shiva <bingishiva3@gmail.com>
2025-05-25 20:54:27 +0530

    Assigments part is done

---
+++ src/Day3/Assignment_Operator_Demo.java
@@ -0,0 +1,24 @@
+package Day3;
+
+public class Assignment_Operator_Demo
+{
+    public static void main(String[] args)
+    {
+     // Short hand operators
+        //int a=10;
+        //int b=a+5;
+
+        int a=10;
+        a+=5;
+        a-=10;
+        a*=2;
+        a/=4;
+        a%=9;
+
+
+        System.out.println(a);
+
+
+
+    }
+}
